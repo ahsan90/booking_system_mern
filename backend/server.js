b51c8732f04d99efc95b742b67e33153a266c1f3
Md Ahsanul Hoque <ahsan90@users.noreply.github.com>
2022-08-07 16:07:09 -0300

    revert changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ const { errroHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const { roleSeedDb } = require('./helper/roleHelper')
 const { defaultRolesAndUsers } = require('./config/defaultRolesAndUsers')
-//const { usersCreated } = require('./config/seed')
 const port = process.env.PORT || 5000
-const User = require('./models/userModel')
 const cors = require('cors')
 const morgan = require('morgan')
 
@@ -28,9 +26,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
+
+/*
 app.use('/', (req, res) => {
     res.send('Welcome to Booking System MERN API')
-}) 
+})*/
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use('/api/profiles', require('./routes/profileRoutes'))
@@ -41,5 +41,3 @@ app.use('/api/auth', require('./routes/authRoutes'))
 //app.use('/api/auth', (req,res)=>{res.send("stuff")})
 app.use(errroHandler)
 
-//console.log(User.create(usersCreated()[0]))
-//console.log(Math.floor(Math.random() * 2))
